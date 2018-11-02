fb910af4f785ea1c4d30defb3e9d6512f8e8ea8a
Sander <44069180+BalloonicornJr@users.noreply.github.com>
2018-11-02 12:06:06 -0700

    Update Obstacle Avoidance 3.py

--- Obstacle Avoidance 3.py
+++ Obstacle Avoidance 3.py
@@ -119,7 +119,7 @@ def dijkstra_search(graph, start, goal):
 def reconstruct_path(came_from, start, goal):
     current = goal
     path = []
-    while current != start and current in points:
+    while current != start:
         path.append(current)
         current = came_from[current] 
     path.append(start) # optional
